5d25345d43156209e185de66df5e16135cda228c
nhcha6 <nhcha6@student.monash.edu>
2018-06-25 12:27:04 +1000

    Tabulated windows test

---
+++ MersProject/StackedWindowsGUI.py
@@ -0,0 +1,99 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QLineEdit, QTextEdit, qApp, QFileDialog, QAction
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 tabs - pythonspot.com'
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 300
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+
+
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+
+        self.show()
+
+
+class MyTableWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Tab 1")
+        self.tabs.addTab(self.tab2, "Tab 2")
+        self.tabs.addTab(self.tab3, "Tab 3")
+
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.pushButton1.clicked.connect(self.showDialog)
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.lineEdit1 = QLineEdit(self)
+        self.tab1.layout.addWidget(self.lineEdit1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Create second tab
+
+
+        #openFile = QAction(QIcon('open.png'), 'Open', self)
+        #openFile.setShortcut('Ctrl+O')
+        #openFile.setStatusTip('Open New File')
+        #openFile.triggered.connect(self.showDialog)
+
+
+
+        #self.setGeometry(300, 300, 350, 300)
+        #self.setWindowTitle('File dialog')
+        #self.show()
+
+
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+    def showDialog(self, parent):
+
+        fname = QFileDialog.getOpenFileName(self, 'Open File', '/home')
+
+        if fname[0]:
+
+            f = open(fname[0], 'r')
+
+            with f:
+
+                data = f.read()
+                self.textEdit.setText(data)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
